c0a08dc5d8f0013ea51e79b06169ef156c0b1b6f
DESKTOP-B3RQNEV\Usuario <1914463@alu.murciaeduca.es>
2025-01-15 17:36:08 +0100

    Paso 4.1 Mejorado

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,4 +1,5 @@
 <h1>{{ $post->title }}</h1>
+<p><strong>Autor:</strong> {{ $post->user->name ?? 'Autor desconocido' }}</p>
 <img src="{{ asset('storage/' . $post->image) }}" alt="{{ $post->title }}" style="max-width: 300px;">
 <p>{{ $post->description }}</p>
 <p>Ingredientes: {{ $post->ingredients }}</p>
