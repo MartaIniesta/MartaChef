a8bf5ae457fa8459c38d2c84346965b3f154667c
DESKTOP-B3RQNEV\Usuario <1914463@alu.murciaeduca.es>
2025-01-15 13:38:28 +0100

    Paso 2

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,6 +8,11 @@ use App\Models\Post;
 
 class PostController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth')->except('index', 'show');
+    }
+
     public function index()
     {
         $posts = Post::all();
