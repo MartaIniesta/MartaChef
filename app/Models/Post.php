a45981a1f2f838c48996d55399a7ad020c05f075
DESKTOP-B3RQNEV\Usuario <1914463@alu.murciaeduca.es>
2025-01-16 20:48:28 +0100

    Paso 5

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Post extends Model
 {
@@ -15,5 +16,15 @@ class Post extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function categories(): belongsToMany
+    {
+        return $this->belongsToMany(Category::class);
+    }
+
+    public function tags(): belongsToMany
+    {
+        return $this->belongsToMany(Tag::class);
     }
 }
