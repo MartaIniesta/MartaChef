650b757f527fcec958c874d3d1cca9fa1e972d41
DESKTOP-B3RQNEV\Usuario <1914463@alu.murciaeduca.es>
2025-02-06 10:03:08 +0100

    Paso 13.1

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,3 +1,1 @@
-<svg viewBox="0 0 316 316" xmlns="http://www.w3.org/2000/svg" {{ $attributes }}>
-    <image href="{{ asset('storage/logo/M.jpeg') }}" x="0" y="0" width="316" height="316" clip-path="circle(50% at 50% 50%)" />
-</svg>
+<img src="{{ asset('storage/logo/logo-MartaChef.png') }}" alt="MartaChef" class="h-28 w-28 object-contain">
